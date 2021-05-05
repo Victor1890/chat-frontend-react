ff1909c5bfeb5ab1d80dc7bc3c176bd3487d50f0
Victor J. Rosario V <victorrosario.1890@gmail.com>
2021-05-04 22:22:36 -0400

    [ADD] first commit

---
+++ src/components/Join/Join.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+import "./Join.css";
+
+export default function SignIn() {
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+
+  return (
+    <div className='joinOuterContainer'>
+      <form className='joinInnerContainer'>
+        <h1 className='heading'>Victor Chat React</h1>
+        <p className='heading_paragraph'>
+          Favor en introducir su nombre de usuario y el ID de la sala de reunión
+        </p>
+        <div>
+          <input
+            placeholder='Nombre de Usuario'
+            className='joinInput'
+            type='text'
+            onChange={(event) => setName(event.target.value)}
+          />
+        </div>
+        <div>
+          <input
+            placeholder='ID de la Sala'
+            className='joinInput mt-20'
+            type='text'
+            onChange={(event) => setRoom(event.target.value)}
+          />
+        </div>
+        <Link
+          onClick={(e) => (!name || !room ? e.preventDefault() : null)}
+          to={`/chat?name=${name}&room=${room}`}
+        >
+          <button className={"button mt-20"}>Registrarse</button>
+        </Link>
+      </form>
+    </div>
+  );
+}
---
+++ src/components/TextContainer/TextContainer.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+import onlineIcon from "../../icons/onlineIcon.png";
+
+import "./TextContainer.css";
+
+const TextContainer = ({ users }) => (
+  <div className='textContainer'>
+    <div>
+      <h1>
+        Aplicación de Chat en tiempo real
+        <span role='img' aria-label='emoji'>
+          💬
+        </span>
+      </h1>
+      <h2>
+        Trata de usarla
+        <span role='img' aria-label='emoji'>
+          ⬅️
+        </span>
+      </h2>
+    </div>
+    {users && (
+      <div>
+        <h1>Personas conectadas:</h1>
+        <div className='activeContainer'>
+          <h2>
+            {users.map(({ name }) => (
+              <h4 key={name} className='activeItem'>
+                {name}
+                <img alt='Online Icon' src={onlineIcon} />
+              </h4>
+            ))}
+          </h2>
+        </div>
+      </div>
+    )}
+  </div>
+);
+
+export default TextContainer;
